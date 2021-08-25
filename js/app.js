212ffe06fd2d6dad83a5ca7e17a58c8663645881
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-08-25 18:32:44 -0400

    added local storage

--- js/app.js
+++ js/app.js
@@ -14,52 +14,10 @@ let middleIndex = null;
 let rightIndex = null;
 let leftIndex = null;
 
-let rounds = 10
+let rounds = 30
 
 var ctx = document.getElementById('my-canvas').getContext('2d');
 
-
-function renderChart() {
-  renderChosen = [];
-  renderLabels = []
-console.log(Product.allProductsArray)
-  for(let product of Product.allProductsArray) {
-    renderChosen.push(product.timesChosen);
-    renderLabels.push(product.name);
-  }
-
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: renderLabels,
-      datasets: [{
-        label: [],
-        data: renderChosen,
-        backgroundColor: [
-          'red', 'orange', 'yellow', 'green', 'blue', 'purple','green','teal', 'grey', 'turquoise', 'violet', 'maroon', 'aqua', 'brown', 'tan', 'beige', 'bisque', 'salmon','black'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-}
-  
-  
   
   
   // ---------------------------------------------------- Constructor Functions -------------------------------------------------------------//
@@ -90,20 +48,19 @@ function randomObject() {
 
   for (let i = 0; i < 3; i++) {
   
-    while (previouslyShown.includes(leftIndex) || leftIndex === middleIndex || leftIndex === rightIndex) {
+    while (previouslyShown.includes(leftIndex)) {
     leftIndex = Math.floor(Math.random() * Product.allProductsArray.length);
    }
     previouslyShown.push(leftIndex);
   
-    while (previouslyShown.includes(middleIndex) || middleIndex === leftIndex || middleIndex === rightIndex) {
+    while (previouslyShown.includes(middleIndex)) {
     middleIndex = Math.floor(Math.random() * Product.allProductsArray.length);
     }
     previouslyShown.push(middleIndex);
   
-    while(previouslyShown.includes(rightIndex) || rightIndex === middleIndex || rightIndex === leftIndex) {
+    while(previouslyShown.includes(rightIndex)) {
     rightIndex = Math.floor(Math.random() * Product.allProductsArray.length);
     }
-    previouslyShown.push(rightIndex);
   }
     let randomLeft = Product.allProductsArray[leftIndex]
     let randomMiddle = Product.allProductsArray[middleIndex]
@@ -142,24 +99,23 @@ function eventHandler(event) {
   }
 
   randomObject();
-
 }
-
 
 
 function buttonEvent(event) {
   if (event.target === viewButton) {
     renderChart();
     renderTable();
+    storeProductLocally();
+    console.log(retrieveProductsLocally);
     viewButton.removeEventListener('click', buttonEvent)
   }
 }
 
 
-
 function renderTable() {
   const ulEl = document.getElementById('click-results')
-  ulEl.innerHTML = '';
+  // ulEl.innerHTML = '';
   for (let product of Product.allProductsArray) {
     const liEl = document.createElement('li')
     liEl.textContent = `${product.name} had ${product.timesChosen} clicks and was shown ${product.timesShown} times.`
@@ -167,7 +123,87 @@ function renderTable() {
   }
 }
 
+function renderChart() {
+  renderChosen = [];
+  renderLabels = []
+console.log(Product.allProductsArray)
+  for(let product of Product.allProductsArray) {
+    renderChosen.push(product.timesChosen);
+    renderLabels.push(product.name);
+  }
 
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: renderLabels,
+      datasets: [{
+        label: [],
+        data: renderChosen,
+        backgroundColor: [
+          '(121,204,179)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
+
+function retrieveProductsLocally() {
+  const packedProducts = localStorage.getItem('products');
+  if(packedProducts !== null) {
+    const parcedProducts = JSON.parse(packedProducts);
+    for( let product of parcedProducts) {
+      const retrievedProduct =   new Product(product.name,product.image)
+      retrievedProduct.timesChosen = parseInt(product.timesChosen);
+      retrievedProduct.timesShown = parseInt(product.timesShown); 
+      Product.allProductsArray.push(retrievedProduct)
+    };
+  } 
+  else {
+
+    Product.allProductsArray.push(new Product('bag', 'assets/bag.jpg'));
+    Product.allProductsArray.push(new Product('banana', 'assets/banana.jpg'));
+    Product.allProductsArray.push(new Product('bathroom', 'assets/bathroom.jpg'));
+    Product.allProductsArray.push(new Product('boots', 'assets/boots.jpg'));
+    Product.allProductsArray.push(new Product('breakfast', 'assets/breakfast.jpg'));
+    Product.allProductsArray.push(new Product('bubblegum', 'assets/bubblegum.jpg'));
+    Product.allProductsArray.push(new Product('chair', 'assets/chair.jpg'));
+    Product.allProductsArray.push(new Product('cthulhu', 'assets/cthulhu.jpg'));
+    Product.allProductsArray.push(new Product('dog-duck', 'assets/dog-duck.jpg'));
+    Product.allProductsArray.push(new Product('dragon', 'assets/dragon.jpg'));
+    Product.allProductsArray.push(new Product('pen', 'assets/pen.jpg'));
+    Product.allProductsArray.push(new Product('pet-sweep', 'assets/pet-sweep.jpg'));
+    Product.allProductsArray.push(new Product('scissors', 'assets/scissors.jpg'));
+    Product.allProductsArray.push(new Product('shark', 'assets/shark.jpg'));
+    Product.allProductsArray.push(new Product('sweep' , 'assets/sweep.png'));
+    Product.allProductsArray.push(new Product('tauntaun', 'assets/tauntaun.jpg'));
+    Product.allProductsArray.push(new Product('unicorn', 'assets/unicorn.jpg'));
+    Product.allProductsArray.push(new Product('water-can', 'assets/water-can.jpg'));
+    Product.allProductsArray.push(new Product('wine-glass', 'assets/wine-glass.jpg'));
+  }
+  randomObject();
+};
+
+function storeProductLocally() {
+  const packedProducts = JSON.stringify(Product.allProductsArray)
+  localStorage.setItem('products', packedProducts)
+};
 
 
 //  --------------------------------------------------- Listeners -------------------------------------------------------------------------//
@@ -178,24 +214,5 @@ viewButton.addEventListener('click', buttonEvent);
 
 // ---------------------------------------------------- Function Calls --------------------------------------------------------------------//
 
-Product.allProductsArray.push(new Product('bag', 'assets/bag.jpg'));
-Product.allProductsArray.push(new Product('banana', 'assets/banana.jpg'));
-Product.allProductsArray.push(new Product('bathroom', 'assets/bathroom.jpg'));
-Product.allProductsArray.push(new Product('boots', 'assets/boots.jpg'));
-Product.allProductsArray.push(new Product('breakfast', 'assets/breakfast.jpg'));
-Product.allProductsArray.push(new Product('bubblegum', 'assets/bubblegum.jpg'));
-Product.allProductsArray.push(new Product('chair', 'assets/chair.jpg'));
-Product.allProductsArray.push(new Product('cthulhu', 'assets/cthulhu.jpg'));
-Product.allProductsArray.push(new Product('dog-duck', 'assets/dog-duck.jpg'));
-Product.allProductsArray.push(new Product('dragon', 'assets/dragon.jpg'));
-Product.allProductsArray.push(new Product('pen', 'assets/pen.jpg'));
-Product.allProductsArray.push(new Product('pet-sweep', 'assets/pet-sweep.jpg'));
-Product.allProductsArray.push(new Product('scissors', 'assets/scissors.jpg'));
-Product.allProductsArray.push(new Product('shark', 'assets/shark.jpg'));
-Product.allProductsArray.push(new Product('sweep' , 'assets/sweep.png'));
-Product.allProductsArray.push(new Product('tauntaun', 'assets/tauntaun.jpg'));
-Product.allProductsArray.push(new Product('unicorn', 'assets/unicorn.jpg'));
-Product.allProductsArray.push(new Product('water-can', 'assets/water-can.jpg'));
-Product.allProductsArray.push(new Product('wine-glass', 'assets/wine-glass.jpg'));
 
-randomObject();
+retrieveProductsLocally();
